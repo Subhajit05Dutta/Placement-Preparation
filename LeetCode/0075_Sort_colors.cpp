f80e756065307901408b8498790f6be578b70958
Subhajit Dutta <subhajit05dutta@gmail.com>
2026-06-14 22:25:31 +0530

    My solution of LC 75 Sort colors

---
+++ LeetCode/0075_Sort_colors.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        int low = -1, mid = 0, high = n - 1;
+        while (mid <= high) {
+            if (nums[mid] == 2) {
+                swap(nums[mid], nums[high]);
+                high--;
+            } else if (nums[mid] == 0) {
+                low++;
+                swap(nums[mid], nums[low]);
+                mid++;
+            } else {
+                mid++;
+            }
+        }
+        return;
+    }
+};
